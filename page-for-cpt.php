f47843d603226be24b714414a2cb49b9d41e959f
Ben Huson <ben@thewhiteroom.net>
2015-02-25 09:55:07 +0000

    Do not require the `page_for_cpt_slug` filter. Automaticatically apply to registered post types.

--- page-for-cpt.php
+++ page-for-cpt.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Page for CPT
 Plugin URI: https://github.com/benhuson/page-for-cpt
-Description: Specify a page to use for the base URL of a custom post type via your WordPress Reading Settings admin page. CPT must support this plugin using the 'page_for_cpt_slug' filter.
+Description: Specify a page to use for the base URL of a custom post type via your WordPress Reading Settings admin page. May not work with post types that specify their own custom permalink structure.
 Author: Ben Huson
 Author URI: https://github.com/benhuson/page-for-cpt
 Version: 0.2
@@ -22,30 +22,11 @@ define( 'PAGE_FOR_CPT_TEXTDOMAIN', 'page-for-cpt' );
 // Don't load if class already exists
 if ( ! class_exists( 'Page_For_CPT' ) ) {
 
-	/**
-	 * Filter custom post type slugs
-	 *
-	 * Apply the 'page_for_cpt_slug' filter on the rewrite argument when registering a post type,
-	 * specifying the default slug to use (if a page is not set for the post type) and the post type:
-	 * apply_filters( 'page_for_cpt_slug', {$default_slug}, {$post_type} )
-	 *
-	 *    $default_slug : Suggest setting to a URL friendly version of the post type.
-	 *    $post_type    : Post type.
-	 *
-	 * 'rewrite' => array(
-	 *    'slug'       => apply_filters( 'page_for_cpt_slug', 'my-post-type', 'my_post_type' ),
-	 *    'with_front' => false,
-	 *    'feeds'      => true,
-	 *    'pages'      => true,
-	 * )
-	 *
-	 * You should also set the post type's 'has_archive' to true.
-	 */
-	add_filter( 'page_for_cpt_slug', array( 'Page_For_CPT', 'page_for_cpt_slug' ), 10, 2 );
-
 	add_action( 'admin_init', array( 'Page_For_CPT', 'init_settings_field' ), 500 );
 
 	add_filter( 'body_class', array( 'Page_For_CPT', 'body_class' ) );
+
+	add_action( 'registered_post_type', array( 'Page_For_CPT', 'registered_post_type' ), 5, 2 );
 
 	/**
 	 * Page for Custom Post Type Class
@@ -58,33 +39,6 @@ if ( ! class_exists( 'Page_For_CPT' ) ) {
 		 * @var  array
 		 */
 		public static $post_types = array();
-
-		/**
-		 * Handle CPT Slug
-		 *
-		 * @param   string  $slug       Default slug.
-		 * @param   string  $post_type  Post type.
-		 * @return  string              Page-based slug.
-		 */
-		public static function page_for_cpt_slug( $slug, $post_type ) {
-
-			// Add post type
-			if ( ! in_array( $post_type, Page_For_CPT::$post_types ) ) {
-				Page_For_CPT::$post_types[] = $post_type;
-			}
-
-			// Get page slug
-			$page_for_cpt = (array) get_option( 'page_for_cpt' );
-			if ( isset( $page_for_cpt[ $post_type ] ) ) {
-				$uri = get_page_uri( $page_for_cpt[ $post_type ] );
-				if ( ! empty( $uri ) ) {
-					$slug = $uri;
-				}
-			}
-
-			return $slug;
-
-		}
 
 		public static function init_settings_field() {
 
@@ -104,9 +58,14 @@ if ( ! class_exists( 'Page_For_CPT' ) ) {
 
 			$page_for_cpt = (array) get_option( 'page_for_cpt' );
 
-			if ( count( Page_For_CPT::$post_types ) > 0 ) {
+			$post_types = array_keys( get_post_types( array(
+				'public'   => true,
+				'_builtin' => false
+			) ) );
+
+			if ( count( $post_types ) > 0 ) {
 				echo '<ul>';
-				foreach ( Page_For_CPT::$post_types as $post_type ) {
+				foreach ( $post_types as $post_type ) {
 
 					// Skip invalid post types
 					if ( ! post_type_exists( $post_type ) ) {
@@ -223,6 +182,91 @@ if ( ! class_exists( 'Page_For_CPT' ) ) {
 			}
 
 			return 0;
+
+		}
+
+		/**
+		 * Registered Post Type
+		 *
+		 * Called by the `registered_post_type` hook when a new post type is registered.
+		 *
+		 * @since  0.3
+		 * @internal
+		 *
+		 * @param  string  $post_type  Post type.
+		 * @param  array   $args       Post type arguments.
+		 */
+		public static function registered_post_type( $post_type, $args ) {
+
+			global $wp_post_types;
+
+			// Get all public custom post types.
+			$post_types = get_post_types( array(
+				'public'   => true,
+				'_builtin' => false
+			) );
+
+			if ( in_array( $post_type, $post_types ) ) {
+
+				$page_for_cpt = (array) get_option( 'page_for_cpt' );
+
+				if ( isset( $page_for_cpt[ $post_type ] ) ) {
+
+					$page_id = absint( $page_for_cpt[ $post_type ] );
+					$uri = get_page_uri( $page_id );
+
+					// If a page is assigned, use that for the rewrite rules.
+					if ( ! empty( $uri ) ) {
+
+						$args->has_archive = $uri;
+
+						$args->rewrite = wp_parse_args( array(
+							'slug'       => $uri,
+							'with_front' => false
+						), (array) $args->rewrite );
+
+					}
+
+					// Rebuild Rewrite Rules
+					// See the register_post_type() function in `wp-includes/post.php`
+					// https://core.trac.wordpress.org/browser/trunk/src/wp-includes/post.php#L1427
+					if ( is_admin() || '' != get_option( 'permalink_structure' ) ) {
+
+						if ( $args->has_archive ) {
+
+							$archive_slug = $args->has_archive === true ? $args->rewrite['slug'] : $args->has_archive;
+
+							if ( $args->rewrite['with_front'] ) {
+								$archive_slug = substr( $wp_rewrite->front, 1 ) . $archive_slug;
+							} else {
+								$archive_slug = $wp_rewrite->root . $archive_slug;
+							}
+
+							add_rewrite_rule( "{$archive_slug}/?$", "index.php?post_type=$post_type", 'top' );
+
+							if ( $args->rewrite['feeds'] && $wp_rewrite->feeds ) {
+								$feeds = '(' . trim( implode( '|', $wp_rewrite->feeds ) ) . ')';
+								add_rewrite_rule( "{$archive_slug}/feed/$feeds/?$", "index.php?post_type=$post_type" . '&feed=$matches[1]', 'top' );
+								add_rewrite_rule( "{$archive_slug}/$feeds/?$", "index.php?post_type=$post_type" . '&feed=$matches[1]', 'top' );
+							}
+
+							if ( $args->rewrite['pages'] ) {
+								add_rewrite_rule( "{$archive_slug}/{$wp_rewrite->pagination_base}/([0-9]{1,})/?$", "index.php?post_type=$post_type" . '&paged=$matches[1]', 'top' );
+							}
+
+						}
+
+						$permastruct_args = $args->rewrite;
+						$permastruct_args['feed'] = $permastruct_args['feeds'];
+						add_permastruct( $post_type, "{$args->rewrite['slug']}/%$post_type%", $permastruct_args );
+
+					}
+
+					$wp_post_types[ $post_type ] = $args;
+
+				}
+
+			}
 
 		}
 
